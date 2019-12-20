39d4f52de02df787b12ea66f4f20e42571adc094
Nitin Anand <nitinjavakid@gmail.com>
2019-12-20 08:40:40 +0530

    Changes to use timer instead of wifi

--- src/config.example.h
+++ src/config.example.h
@@ -1,10 +1,10 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define WIFI_SSID     ""
-#define WIFI_PASSWORD ""
-#define WIFI_IP       "192.168.1.100"
-#define WIFI_GATEWAY  "192.168.1.2"
-#define WIFI_NETMASK  "255.255.255.0"
+#define MOTOR_PIN "C0"
+#define LED_PIN   "C1"
+#define LEVEL1_PIN "C2"
+#define LEVEL2_PIN "C3"
+#define INTERVAL_HOURS 24
 
 #endif
